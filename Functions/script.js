553c59c1de9ff5627f1ce0f6f941a9562a51e0aa
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-07-21 15:56:40 +0530

    Functions in JavaScript

---
+++ Functions/script.js
@@ -0,0 +1,39 @@
+// // Function 
+
+
+// function greet(){
+//     console.log("Hello");
+// }
+
+// greet();
+
+
+// Function expression
+
+// const greet = function(){
+//     console.log("Hello")
+// }
+
+// console.log(greet());
+
+
+// Arrow functions
+
+// const greet = () => {
+//     console.log("Hello, I am an arrow function")
+// }
+
+// greet();
+
+// Immediately Invoked Function Expression - IIFE
+// (function(){
+//     console.log("I am IIFE")
+// })();
+
+
+// Anonymous Functions
+// setTimeout(function, name)
+
+setTimeout(() => {
+    console.log("I am an anonymous function");
+},2000);
