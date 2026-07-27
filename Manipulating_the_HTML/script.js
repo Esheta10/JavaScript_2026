c9ec5c16a21a09f27d3e63c6b1bacc83ba78190d
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-07-28 01:43:59 +0530

    Inserting elements

--- Manipulating_the_HTML/script.js
+++ Manipulating_the_HTML/script.js
@@ -1,11 +1,30 @@
-let h1 = document.querySelector("h1")
-// console.log(h1.tagName)
-// console.log(h1.nodeName)
-// console.log(h1.firstChild)
+// let h1 = document.querySelector("h1")
+// // console.log(h1.tagName)
+// // console.log(h1.nodeName)
+// // console.log(h1.firstChild)
 
 
-console.log(`innerText:  ${h1.innerText}`)
-console.log(`textContent: ${h1.textContent}`)
-console.log(`innnerHTML: ${h1.innerHTML}`)
+// console.log(`innerText:  ${h1.innerText}`)
+// console.log(`textContent: ${h1.textContent}`)
+// console.log(`innnerHTML: ${h1.innerHTML}`)
 
+// div.append(`<p>I will be added inside but at the end of node</p>`)
 
+// div.prepend(`<p>I will be added inside but at the start of node</p>>`)
+
+// div.after(`<p>I will be added outside after the node</p>`)
+
+// div.before(`<p>I will be added outside before the node</p>`)
+
+// div.remove()
+
+// let elem = document.createElement('p')
+// elem.innerText = "Insert Adjacent Element"
+
+// div.insertAdjacentElement("beforeEnd", elem)
+
+let div = document.querySelector("div")
+console.log(div);
+
+let para1 = document.querySelector(".para-1")
+para1.style.backgroundColor = "blue"
