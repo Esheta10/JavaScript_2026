c7fc9e41820917efc70568d73eb5094aa4a1d333
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-07-27 21:18:22 +0530

    DOM Manipulation, understanding NodeList and HTMLCollection

---
+++ DOM_Manipulation/script.js
@@ -0,0 +1,21 @@
+// let button = document.querySelector("button")
+
+// button.addEventListener("click",()=>{
+//     button.style.backgroundColor = "red"
+// })
+
+// let h1 =  document.getElementsByTagName("h1")
+// console.log(h1)
+
+// let p = document.getElementsByClassName("p")
+// console.log(p)
+
+// let heading = document.getElementById("heading")
+// console.log(heading)
+
+
+let heading  = document.querySelector("#heading")
+console.log(heading)
+
+console.log(heading.firstChild)
+console.log(heading.lastChild)
