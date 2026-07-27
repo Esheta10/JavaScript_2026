87daa63b792daeed8f665464786636a364afbce8
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-07-27 19:06:45 +0530

    Basics of DOM Manipulation

---
+++ DOM_Basics/script.js
@@ -0,0 +1,35 @@
+console.log("Hello JavaScript!")
+// let h1 = document.querySelector("h1")
+// h1.innerHTML = "Hello Universe!"
+
+// let p = document.querySelector("p")
+// p.innerHTML = "You have to burn like a sun in order to shine like a Sun."
+
+// let a = document.querySelector("h1")
+// a.style.backgroundColor = "maroon"
+
+// let b = document.querySelector("p")
+// b.style.backgroundColor="green"
+
+// let h1 = document.querySelector("h1")
+// // addEventListener takes 2 parameters ---> event and function
+// h1.addEventListener("click", ()=>{
+//     h1.style.backgroundColor = "pink"
+//     h1.style.color = "black"
+// })
+
+
+let box = document.querySelector("div")
+box.addEventListener("click",()=>{
+    box.style.backgroundColor =  "yellow";
+})
+
+let off = document.querySelector(".off-button")
+off.addEventListener("click", ()=>{
+    box.style.backgroundColor = "red";
+})
+
+let on = document.querySelector(".on-button")
+on.addEventListener("click", ()=>{
+    box.style.backgroundColor = "green";
+})
