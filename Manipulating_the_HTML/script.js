22029fce072bd9bf7005611bcb7eea44441f6d50
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-07-28 00:35:25 +0530

    innerText, innerHTML and textContent

---
+++ Manipulating_the_HTML/script.js
@@ -0,0 +1,11 @@
+let h1 = document.querySelector("h1")
+// console.log(h1.tagName)
+// console.log(h1.nodeName)
+// console.log(h1.firstChild)
+
+
+console.log(`innerText:  ${h1.innerText}`)
+console.log(`textContent: ${h1.textContent}`)
+console.log(`innnerHTML: ${h1.innerHTML}`)
+
+
