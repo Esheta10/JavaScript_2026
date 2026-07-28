b77a85f665c9fe82e7a71fe8ad39f6878fe3fdd6
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-07-28 18:17:16 +0530

    Manipulating the CSS

---
+++ Manipulating_the_CSS/script.js
@@ -0,0 +1,20 @@
+let div = document.querySelector(".div")
+
+// Using style
+div.style.backgroundColor = "red"
+div.style.fontSize = "60px"
+
+// Using attribute method
+div.setAttribute('style','background-color: salmon; color: blue; font-size: 100px; border: 2px solid black')
+
+// Using cssText property
+div.style.cssText = "background-color: maroon; color: bisque; font-style: italic; font-size: 50px; padding-left: 450px;"
+
+// Using className property
+console.log(div.className);
+
+// Using clasList property : add(), remove(), toggle(), contains()
+div.classList.add("maroon-bg")
+div.classList.add("bisque-text-color")
+div.classList.remove("maroon-bg")
+console.log(div.classList.contains("bisque-text-color"))
